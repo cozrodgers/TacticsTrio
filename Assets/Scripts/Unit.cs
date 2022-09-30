78b94342e1ee7c1aa2e9ad178e6b9ddfeb5bc867
cozrodgers <coreyrodgers@gmail.com>
2022-09-30 23:06:00 +1000

    add animation logic for walking when moving to mouse world position

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     [SerializeField] private Vector3 targetPosition;
+    [SerializeField] private Animator unitAnimator;
 
     private void Update()
     {
         float stoppingDistance = 0.1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
+
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * Time.deltaTime * moveSpeed;
+            unitAnimator.SetBool("IsWalking", true);
+        } else {
+            
+            unitAnimator.SetBool("IsWalking", false);
         }
-       if(Input.GetMouseButtonDown(0)) {
-        Move(MouseWorld.GetPosition());
-       }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Move(MouseWorld.GetPosition());
+        }
     }
     void Move(Vector3 targetPosition)
     {
